0ccaf89b8154dedc2d451e95be44e30cd1c8074e
Emil Schutte <emilschutte@gmail.com>
2022-09-07 16:07:47 +0200

    Support synchronizing autosave via `requestVersionAutoSave`

--- index.js
+++ index.js
@@ -9,16 +9,17 @@
 
 const
     { WebSocketServer } = require('./src/server.js'),
-    args                = process.argv.slice(2).reduce((acc, arg) => {
+    args                 = process.argv.slice(2).reduce((acc, arg) => {
         const [k, v = true] = arg.split('=');
         acc[k] = v;
         return acc;
     }, {}),
-    resetDelay          = (args.resetDelay || 30 * 60) * 1000,
-    debug               = args.debug,
-    port                = args.port || 8080;
+    resetDelay           = (args.resetDelay || 30 * 60) * 1000,
+    debug                = args.debug,
+    port                 = args.port || 8080,
+    autoSaveIntervalMins = args.autoSaveIntervalMins;
 
-const server = new WebSocketServer({ debug, resetDelay, port });
+const server = new WebSocketServer({ debug, resetDelay, port, autoSaveIntervalMins });
 
 server.init(port).then(() => {
     server.showWebSocketServerAddress();
