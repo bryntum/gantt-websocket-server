8cbfc0265916efb1147c99c415a30c9a887586b1
Maxim Gorkovskii <ragzovskii@live.ru>
2026-05-03 00:20:29 +0400

    pass revision type

--- src/server/MessageHandler.js
+++ src/server/MessageHandler.js
@@ -223,7 +223,8 @@ class MessageHandler extends AuthorizationHandler {
                 revision      : this.getNextRevision(project),
                 localRevision : revision.revision,
                 client        : ws.id,
-                changes       : this.dataHandler.handleProjectChanges(project, revision.changes).changes
+                changes       : this.dataHandler.handleProjectChanges(project, revision.changes).changes,
+                revisionType  : revision.revisionType,
             };
 
             if (revision.conflictResolutionFor) {
